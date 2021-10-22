ec5cf6d5f3848422f1d2194b4fec6146d13122f3
Marcelo Alcaraz <marcelo@elumen.info>
2021-10-22 12:37:49 -0300

    agregar pista *

--- src/codebreaker/codebreaker.spec.js
+++ src/codebreaker/codebreaker.spec.js
@@ -10,4 +10,14 @@ describe("Codebreaker logic", () => {
     const cb = new CodeBreaker(3);
     expect(cb.arriesgar(5)).not.toEqual("Ganaste!");
   });
+
+  it("deber recibir '*' si un digito coincide", () => {
+    const cb = new CodeBreaker(53);
+    expect(cb.arriesgar(54)).toEqual("*");
+  });
+
+  it("no debe recibir pistas si ningun digito coincide", () => {
+    const cb = new CodeBreaker(53);
+    expect(cb.arriesgar(64).includes("*")).toBe(false);
+  });
 });
